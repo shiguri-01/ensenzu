ff9e07745c41ad2d42d1d80b7c694f3d02f09290
shiguri <131871107+shiguri-01@users.noreply.github.com>
2026-06-28 15:01:28 +0900

    chore: Cloudflareのいろいろを追加

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import rabbita from "@rabbita/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-    plugins: [rabbita({ mainPkgDir: "src/cmd/main" })],
+  plugins: [rabbita({ mainPkgDir: "src/cmd/main" }), cloudflare()],
 });
